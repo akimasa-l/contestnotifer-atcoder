5d4f8ef23f4963c2facb4d2ca5bda591f8e6919e
Akimasa_L <limingzheng8@outlook.com>
2020-09-17 16:31:01 +0900

    get well in making json

--- find_contest.py
+++ find_contest.py
@@ -2,20 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import re
+import json
 
-url="https://atcoder.jp/contests/?lang=en"
 
-r=requests.get(url)
-b=BeautifulSoup(r.text,"html.parser")
-table=b.find(id="contest-table-upcoming")
-for tr in table.find_all("tr"):
-    contest=tr.find_all("td")
-    if not contest:
-        continue
-    cname=contest[1].find("a").text
-    ctime=contest[0].find("time").text
-    cdtime=contest[2].text
-    rated=contest[3].text
-    year,month,day,hour,minutes,se,plus=re.split(r"[\-:\+ ]",ctime)
-    dhour,dminutes=cdtime.split(":")
-    print(cname,ctime,cdtime,rated)
+def get_contest(lang):
+    c=[]
+    url="https://atcoder.jp/contests/?lang="+lang
+    
+    r=requests.get(url)
+    b=BeautifulSoup(r.text,"html.parser")
+    table=b.find(id="contest-table-upcoming")
+    for tr in table.find_all("tr"):
+        contest=tr.find_all("td")
+        if not contest:
+            continue
+        cname=contest[1].find("a").text
+        curl="https://atcoder.jp"+contest[1].find("a").get("href")
+        ctime=contest[0].find("time").text
+        cdtime=contest[2].text
+        rated=contest[3].text
+        year,month,day,hour,minute,second,gmt=re.split(r"[\-:\+ ]",ctime)
+        dhour,dminute=cdtime.split(":")
+        #print(cname,ctime,cdtime,rated,curl)
+        contestdict={
+            "name":cname,
+            "date":{
+                "year":year,
+                "month":month,
+                "day":day,
+                "hour":hour,
+                "minute":minute,
+                "second":second,
+                "gmt":gmt
+            },
+            "dtime":{
+                "hour":dhour,
+                "minute":dminute
+            },
+            "rated":rated,
+            "url":curl
+        }
+        c.append(contestdict)
+    return c
+
+print(*get_contest("en"))
