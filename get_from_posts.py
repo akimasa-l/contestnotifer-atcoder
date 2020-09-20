efac6951cccfddc7a53306a632c5789bde10094e
Akimasa_L <limingzheng8@outlook.com>
2020-09-20 14:44:22 +0900

    いろいろ

--- get_from_posts.py
+++ get_from_posts.py
@@ -5,21 +5,24 @@ import datetime
 import re
 import json
 
-url="https://atcoder.jp/posts/?lang=ja"
-r=requests.get(url)
-b=BeautifulSoup(r.text,"html.parser")
-c=[]
-for info in b.select(".panel-default"):
-    cname=info.find("a").text
-    if not(re.match(".*告知",cname)):
-        continue
-    cname=cname.replace(" 告知","")
-    li=info.text.split("- ")
-    pronum=li[4].split()
-    writers=li[5][7:]
-    #print(writers)
-    ws=[writer.text for writer in BeautifulSoup(writers,"html.parser").find_all("span")]
-    point=re.search("配点は .* です。",info.text).group()#[4:-4]
-    print(cname,pronum,ws,point)
 
-print(c)
+def get_from_posts():
+    url="https://atcoder.jp/posts/?lang=ja"
+    r=requests.get(url)
+    b=BeautifulSoup(r.text,"html.parser")
+    c=[]
+    for info in b.select(".panel-default"):
+        cname=info.find("a").text
+        if not(re.match(".*告知",cname)):
+            continue
+        cname=cname.replace(" 告知","")
+        li=info.text.split("- ")
+        pronum=li[4].split()
+        writers=li[5][7:]
+        #print(writers)
+        ws=[writer.text for writer in BeautifulSoup(writers,"html.parser").find_all("span")]
+        point=re.search("配点は .* です。",info.text).group()#[4:-4]
+        #print(cname,pronum,ws,point)
+        c.append({"cname":cname,"problemnumber":pronum,"writer":ws,"point":point})
+    #print(c)
+    return c
