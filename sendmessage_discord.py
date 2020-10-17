3ec16872103c57ea5672bdcb66a212295d46b8f0
Akimasa Liu <liumingzheng8@outlook.com>
2020-10-17 14:52:08 +0900

    discordに対応しました！

--- sendmessage_discord.py
+++ sendmessage_discord.py
@@ -1,38 +1,22 @@
-import discord
-import requests
 import json
+import requests
 
-with open("../../discord/accesstoken.txt") as f:
-    token=f.read().rstrip()
+with open("../../discord/to.txt") as f:
+    urls=f.read().split()
 
-client = discord.Client()
+with open("./messages.json") as f:
+    contests=json.load(f)
 
-color={0:"gray",400:"brown",800:"green",1200:"cyan",1600:"blue",2000:"yellow"}
+def sendmessage(url,message):
+    params={
+        "content":message,
+    }
+    headers={"Content-Type":"application/json"}
+    a=requests.post(url,data=json.dumps(params),headers=headers)
+    print(a.status_code)
+    print(a.text)
 
-async def getcolor(atcoderId):
-    url=f"https://atcoder.jp/users/{atcoderId}/history/json"
-    a=requests.get(url)
-    d=json.loads(a.text)
-    if d:
-        pass
-    else:
-        return "black"
-
-async def isExist(atcoderId):
-    url=f"https://atcoder.jp/users/{atcoderId}/"
-    a=requests.get(url)
-    if a.status_code==200:
-        return True
-    return False
-
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    a=message.content.split()
-    if len(a)!=2:
-        return
-    if a[0]=="!identify":
-        await isExist(a[1])
-
-client.run(token)
+if __name__ == "__main__":
+    for url in urls:
+        for contest in contests:
+            sendmessage(url,contest)
