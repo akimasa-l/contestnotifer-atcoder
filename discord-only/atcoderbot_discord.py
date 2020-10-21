9d467e1ecfa580485854e99702e3337d366897e7
Akimasa Liu <liumingzheng8@outlook.com>
2020-10-21 18:44:30 +0900

    colors.iniを追加しました

--- discord-only/atcoderbot_discord.py
+++ discord-only/atcoderbot_discord.py
@@ -13,10 +13,10 @@ with open("../../discord/dburl.txt") as f:
 
 client = discord.Client()
 
-colors={1:"black",400:"gray",800:"brown",1200:"green",1600:"cyan",2000:"blue",2400:"yellow",2800:"orange",3200:"red"}
+ratingcolors={1:"black",400:"gray",800:"brown",1200:"green",1600:"cyan",2000:"blue",2400:"yellow",2800:"orange",3200:"red"}
 
 def getcolor(rating):
-    for i,j in colors.items():
+    for i,j in ratingcolors.items():
         if rating<i:
             return j
 
---
+++ discord-only/create_atcoder_role.py
@@ -0,0 +1,17 @@
+#API reference
+#https://discordpy.readthedocs.io/ja/latest/api.html?highlight=role#discord.Guild.create_role
+import discord
+import configparser
+
+config=configparser.ConfigParser()
+with open("./colors.ini") as f:
+    config.read_file(f)
+
+with open("../../discord/accesstoken.txt") as f:
+    token=f.read().rstrip()
+
+client = discord.Client()
+
+
+
+client.run(token)
