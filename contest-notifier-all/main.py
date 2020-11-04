dac9ac5f9680879b201b81bb0428fbc5ea4e023c
Akimasa_L <limingzheng8@outlook.com>
2020-11-04 15:03:40 +0900

    main.pyを作りました

--- contest-notifier-all/main.py
+++ contest-notifier-all/main.py
@@ -1,3 +1,11 @@
-python3 ./find_contest.py
-python3 ./get_from_posts.py
-python3 ./merge.py
+import subprocess
+import os
+files=(
+    "./find_contest.py",
+    "./get_from_posts.py",
+    "./merge.py"
+)
+for file in files:
+    subprocess.run("python3 " +file,shell=True)
+    #os.system("python3 " +file)
+    print("Done")
