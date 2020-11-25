6d787859ddfec3ed3e38026144ecb891ed22b3f9
Akimasa Liu <liumingzheng8@outlook.com>
2020-11-25 20:32:00 +0900

    Updated on Raspberry pi

--- contest-notifier-all/main.py
+++ contest-notifier-all/main.py
@@ -5,8 +5,20 @@ files=(
     "./get_from_posts.py",
     "./merge.py"
 )
-subprocess.run("git pull origin master")
+commit=lambda:subprocess.run('git commit -am "Updated on Raspberry pi"')
+git_control=lambda c:subprocess.run(f"git {c} origin master")
+pull=lambda:git_control("pull")
+push=lambda:git_control("push")
+
+commit()
+pull()
+commit()
+push()
+
 for file in files:
     subprocess.run("python3 " +file,shell=True)
     #os.system("python3 " +file)
     print("Done")
+
+commit()
+push()
