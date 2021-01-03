1e3ca6b0a8b105340f605507bb3997011d24d864
Akimasa Liu <liumingzheng8@outlook.com>
2021-01-03 22:59:08 +0900

    これでprocessをkillできるようになりました

--- discord-only/kill_atcoderbot_discord.py
+++ discord-only/kill_atcoderbot_discord.py
@@ -1,8 +1,9 @@
+import platform
+import os
 def main():
-    import platform
     if platform.system()!="Linux":
         exit()
     import psutil
     for proc in psutil.process_iter(['pid', 'name', 'username',"cmdline"]):
-        if proc.info["cmdline"]==['/usr/bin/python3', 'atcoderbot_discord.py']:
+        if proc.info["cmdline"]==['/usr/bin/python3', 'atcoderbot_discord.py'] and int(proc.info["pid"])!=int(os.getpid()):
             proc.kill()
