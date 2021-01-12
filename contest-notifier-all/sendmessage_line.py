8ed860de08447bba0ec0d0fca2003a735d7f416b
Akimasa Liu <liumingzheng8@outlook.com>
2021-01-12 21:41:00 +0900

    idと入力するとコンテスト状況をお知らせします。

--- contest-notifier-all/sendmessage_line.py
+++ contest-notifier-all/sendmessage_line.py
@@ -22,8 +22,14 @@ def sendmessage(to,contest):
     print(h.text)
 
 def gettoto():
+    """
     with open("../../line/to.txt") as f:
         toto=f.read().split()
+    """
+    with open("../../line/dburl.txt") as f:
+        dburl=f.read().rstrip()
+    a=requests.get(dburl)
+    toto=json.loads(a.text)
     return toto
 
 for contest in contests:
